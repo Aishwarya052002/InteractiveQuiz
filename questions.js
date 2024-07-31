57201400037e1aa368e4996e9fd9495e0ea333b7
Aishwarya052002 <aishwaryatalekar05@gmail.com>
2024-07-31 14:28:49 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which HTTP method is used to retreive data from a server?",
+    answer: "GET",
+    options: [
+      "POST",
+      "GET",
+      "PUT",
+      "DELETE"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stnds for",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the latest version of HTML as of 2024?",
+    answer: "HTML5",
+    options: [
+      "XHTML",
+      "HTML5",
+      "HTMLX",
+      "XML"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does API stand for?",
+    answer: "Application Programming Interface",
+    options: [
+      "Active Programming Interface",
+      "Application Programming Interface",
+      "Application Protocol Interface",
+      "None of the above"
+    ]
+  },
+];
